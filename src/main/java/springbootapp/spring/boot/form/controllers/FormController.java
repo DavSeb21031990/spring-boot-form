0d6b591b75f2e27778d7c4f96eab8e3cb1b3f58e
DavSeb21031990 <72324297+DavSeb21031990@users.noreply.github.com>
2022-07-10 10:25:03 -0500

    Usar la implementacion automatica de errores, usar la etiqueta th:errors para mostrar el error

--- src/main/java/springbootapp/spring/boot/form/controllers/FormController.java
+++ src/main/java/springbootapp/spring/boot/form/controllers/FormController.java
@@ -3,15 +3,11 @@ package springbootapp.spring.boot.form.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import springbootapp.spring.boot.form.models.domain.Usuario;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class FormController {
@@ -28,11 +24,6 @@ public class FormController {
     public String procesar(@Valid Usuario usuario, BindingResult result, Model model){
 
         if(result.hasErrors()){
-            Map<String, String> errores = new HashMap<>();
-            result.getFieldErrors().forEach(error -> {
-                errores.put(error.getField(), "El campo ".concat(error.getField()).concat(" ").concat(error.getDefaultMessage()));
-            });
-            model.addAttribute("error", errores);
             return "form";
         }
 
