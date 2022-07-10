c07ad2afafb4014b0da63e6423bf33531892f5a8
DavSeb21031990 <72324297+DavSeb21031990@users.noreply.github.com>
2022-07-09 22:51:42 -0500

    Agregar validacion en el metodo handler que recibe los datos del formulario Mostrar los mensajes de error en el formulario

--- src/main/java/springbootapp/spring/boot/form/controllers/FormController.java
+++ src/main/java/springbootapp/spring/boot/form/controllers/FormController.java
@@ -2,22 +2,39 @@ package springbootapp.spring.boot.form.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import springbootapp.spring.boot.form.models.domain.Usuario;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class FormController {
 
     @GetMapping("/form")
     public String formModel(Model model){
+        Usuario usuario = new Usuario();
         model.addAttribute("titulo", "Formulario Usuarios");
+        model.addAttribute("user", usuario);
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(Usuario usuario, Model model){
+    public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model){
+
+        if(result.hasErrors()){
+            Map<String, String> errores = new HashMap<>();
+            result.getFieldErrors().forEach(error -> {
+                errores.put(error.getField(), "El campo ".concat(error.getField()).concat(" ").concat(error.getDefaultMessage()));
+            });
+            model.addAttribute("error", errores);
+            return "form";
+        }
 
         model.addAttribute("titulo", "Resultado form");
         model.addAttribute("usuario", usuario);
--- src/main/java/springbootapp/spring/boot/form/models/domain/Usuario.java
+++ src/main/java/springbootapp/spring/boot/form/models/domain/Usuario.java
@@ -1,9 +1,15 @@
 package springbootapp.spring.boot.form.models.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Usuario {
 
