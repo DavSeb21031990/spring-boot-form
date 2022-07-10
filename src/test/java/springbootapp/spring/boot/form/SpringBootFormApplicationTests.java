0236e57e8cafa5dd5c088cc085f995513fde7bde
DavSeb21031990 <72324297+DavSeb21031990@users.noreply.github.com>
2022-07-09 19:54:28 -0500

    Crear proyecto spring-boot-form Agregar controller y metodos handler para mostrar formulario y enviar datos Agregar dependencia spring-boot-starter-validation

---
+++ src/test/java/springbootapp/spring/boot/form/SpringBootFormApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootapp.spring.boot.form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
